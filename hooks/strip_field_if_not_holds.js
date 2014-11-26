b7a1ca6ebb297db6540b40ffe7732408ab465deb
Tim Kuijsten <tim@netsend.nl>
2014-11-26 19:57:22 +0100

    use new match-object npm

--- hooks/strip_field_if_not_holds.js
+++ hooks/strip_field_if_not_holds.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // use opts.field and opts.fieldFilter to check if a field should be stripped
-var match = require('./../lib/match');
+var match = require('match-object');
 module.exports = function(db, item, opts, cb) {
   if (typeof item[opts.field] !== 'undefined') {
     if (!match(opts.fieldFilter, item)) {
