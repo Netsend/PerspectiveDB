f188e122e628901cb418ebc36f4316d2e09a3870
Tim Kuijsten <tim@netsend.nl>
2014-12-12 16:27:27 +0100

    vcexec: fix sending pull requests

--- test/lib/versioned_collection_exec_root.js
+++ test/lib/versioned_collection_exec_root.js
@@ -24,6 +24,7 @@ if (process.getuid() !== 0) {
 }
 
 var assert = require('assert');
+var net = require('net');
 var childProcess = require('child_process');
 
 var async = require('async');
@@ -113,6 +114,71 @@ tasks.push(function(done) {
       break;
     case 'listen':
       child.kill();
+      break;
+    }
+  });
+});
+
+// should pass through a pull request
+tasks.push(function(done) {
+  var child = childProcess.fork(__dirname + '/../../lib/versioned_collection_exec', { silent: true });
+
+  var stderr = '';
+
+  child.stderr.setEncoding('utf8');
+  child.stderr.pipe(process.stderr);
+  child.stderr.on('data', function(data) {
+    stderr += data;
+  });
+
+  var host = '127.0.0.1';
+  var port = 1234;
+
+  // start server to check if pull request is sent by vcexec
+  var server = net.createServer(function(conn) {
+    conn.on('data', function(data) {
+      assert.deepEqual(JSON.parse(data), {
+        username: 'foo',
+        password: 'bar',
+        database: 'baz',
+        collection: 'qux',
+        offset: null
+      });
+      server.close();
+      child.kill();
+    });
+  });
+  server.listen(port, host);
+
+  child.on('exit', function(code, sig) {
+    assert.strictEqual(stderr.length, 0);
+    assert.strictEqual(code, 0);
+    assert.strictEqual(sig, null);
+    done();
+  });
+
+  child.send({
+    dbName: 'test',
+    dbPort: 27019,
+    collectionName: 'test',
+    chrootUser: 'nobody',
+    chrootNewRoot: '/var/empty'
+  });
+
+  child.on('message', function(msg) {
+    switch (msg) {
+    case 'init':
+      break;
+    case 'listen':
+      // send pr
+      child.send({
+        username: 'foo',
+        password: 'bar',
+        database: 'baz',
+        collection: 'qux',
+        host: host,
+        port: port
+      });
       break;
     }
   });
