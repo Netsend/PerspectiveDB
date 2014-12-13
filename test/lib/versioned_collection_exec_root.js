e5036c5ae0e9bc9ff7560d6264dcb8bd45854ddc
Tim Kuijsten <tim@netsend.nl>
2014-12-13 15:28:37 +0100

    vcexec: fix handling incoming data when pr is sent

--- test/lib/versioned_collection_exec_root.js
+++ test/lib/versioned_collection_exec_root.js
@@ -26,11 +26,27 @@ if (process.getuid() !== 0) {
 var assert = require('assert');
 var net = require('net');
 var childProcess = require('child_process');
+var mongodb = require('mongodb');
+var BSON = mongodb.BSON;
+var Timestamp = mongodb.Timestamp;
 
 var async = require('async');
 
 var tasks = [];
 var tasks2 = [];
+
+var db;
+var databaseName = 'test_versioned_collection_exec_root';
+var Database = require('../_database');
+
+// open database connection
+var database = new Database(databaseName);
+tasks.push(function(done) {
+  database.connect(function(err, dbc) {
+    db = dbc;
+    done(err);
+  });
+});
 
 // should require chrootUser to have a valid username
 tasks.push(function(done) {
@@ -48,7 +64,7 @@ tasks.push(function(done) {
   });
 
   child.send({
-    dbName: 'test',
+    dbName: 'test_versioned_collection_exec_root',
     dbPort: 27019,
     collectionName: 'test',
     chrootUser: 'test',
@@ -72,7 +88,7 @@ tasks.push(function(done) {
   });
 
   child.send({
-    dbName: 'test',
+    dbName: 'test_versioned_collection_exec_root',
     dbPort: 27019,
     collectionName: 'test',
     chrootUser: 'nobody',
@@ -101,7 +117,7 @@ tasks.push(function(done) {
   });
 
   child.send({
-    dbName: 'test',
+    dbName: 'test_versioned_collection_exec_root',
     dbPort: 27019,
     collectionName: 'test',
     chrootUser: 'nobody',
@@ -158,7 +174,7 @@ tasks.push(function(done) {
   });
 
   child.send({
-    dbName: 'test',
+    dbName: 'test_versioned_collection_exec_root',
     dbPort: 27019,
     collectionName: 'test',
     chrootUser: 'nobody',
@@ -183,6 +199,125 @@ tasks.push(function(done) {
     }
   });
 });
+
+// should save valid incoming BSON data following a pull request
+tasks.push(function(done) {
+  // then fork a vce
+  var child = childProcess.fork(__dirname + '/../../lib/versioned_collection_exec', { silent: true });
+
+  // start an echo server that can receive auth requests and sends some BSON data
+  var host = '127.0.0.1';
+  var port = 1234;
+
+  // start server to check if pull request is sent by vcexec
+  var server = net.createServer(function(conn) {
+    conn.on('data', function(data) {
+      assert.deepEqual(JSON.parse(data), {
+        username: 'foo',
+        password: 'bar',
+        database: 'baz',
+        collection: 'qux',
+        offset: null
+      });
+
+      // now send a root node
+      var obj = { _id: { _id: 'abc', _v: 'def', _pa: [] } };
+      conn.write(BSON.serialize(obj));
+
+      setTimeout(function() {
+        // open and search in database
+        db.collection('m3.test').find().toArray(function(err, items) {
+          if (err) { throw err; }
+
+          assert.strictEqual(items.length, 2);
+          assert.deepEqual(items[0], {
+            _id: {
+              _id: 'abc',
+              _v: 'def',
+              _pa: [],
+              _pe: 'baz',
+              _co: 'test'
+            },
+            _m3: {
+              _ack: false,
+              _op: new Timestamp(0, 0)
+            }
+          });
+          assert.deepEqual(items[1], {
+            _id: {
+              _id: 'abc',
+              _v: 'def',
+              _pa: [],
+              _pe: '_local',
+              _co: 'test',
+              _i: 1
+            },
+            _m3: {
+              _ack: false,
+              _op: new Timestamp(0, 0)
+            }
+          });
+
+          server.close();
+          child.kill();
+        });
+      }, 200);
+    });
+  });
+  server.listen(port, host);
+
+  var vcCfg = {
+    dbName: 'test_versioned_collection_exec_root',
+    dbPort: 27019,
+    debug: false,
+    collectionName: 'test',
+    chrootUser: 'nobody',
+    chrootNewRoot: '/var/empty',
+    autoProcessInterval: 50,
+    size: 1,
+    remotes: ['baz'], // list PR sources
+  };
+
+  var pr = {
+    username: 'foo',
+    password: 'bar',
+    database: 'baz',
+    collection: 'qux',
+    host: host,
+    port: port
+  };
+
+  var stderr = '';
+
+  child.stdout.setEncoding('utf8');
+
+  child.stderr.setEncoding('utf8');
+  child.stderr.pipe(process.stderr);
+  child.stderr.on('data', function(data) {
+    stderr += data;
+  });
+
+  child.on('exit', function(code, sig) {
+    assert.strictEqual(stderr.length, 0);
+    assert.strictEqual(code, 0);
+    assert.strictEqual(sig, null);
+    done();
+  });
+
+  child.send(vcCfg);
+
+  child.on('message', function(msg) {
+    switch (msg) {
+    case 'init':
+      break;
+    case 'listen':
+      child.send(pr);
+      break;
+    }
+  });
+});
+
+tasks.push(database.disconnect.bind(database));
 
 async.series(tasks, function(err) {
   if (err) {
