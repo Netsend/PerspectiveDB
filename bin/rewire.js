f9e8a3a953c863f99f1c2d8556c62cb0870e526b
Daan Kuijsten <code@kuijstenwebcompany.nl>
2015-01-04 18:43:27 +0100

    rewire.js with ini config

--- bin/rewire.js
+++ bin/rewire.js
@@ -21,13 +21,15 @@
 'use strict';
 
 var program = require('commander');
+var properties = require('properties');
+var fs = require('fs');
 
 var _db = require('./_db');
 var VersionedCollection = require('../lib/versioned_collection');
 
 program
   .version('0.0.1')
-  .usage('[-v] -d database -c collection src dst parents...')
+  .usage('[-v] -c collection -f config src dst parents...')
   .description('copy a version to a new version and use certain parents')
   .option('-d, --database <database>', 'name of the database')
   .option('-c, --collection <collection>', 'name of the collection to repair')
@@ -36,7 +38,7 @@ program
   .option('dst', 'destination version')
   .option('parents', 'one or more parent versions to set on the new item')
   .option('')
-  .option('    --config <file>', 'config file with db parameters, defaults to config/production.json')
+  .option('-f, --config <config>', 'an ini config file')
   .option('-p, --pe', 'perspective, defaults to local perspective')
   .option('-n  --new-version <version>', 'new version, defaults to dst or random if the same as src')
   .option('-s, --save', 'insert the new item in the snapshot')
@@ -47,29 +49,28 @@ if (!program.args[1]) {
   program.help();
 }
 
-var config = program.config || __dirname + './../config/production.json';
+if (!program.config) { program.help(); }
+if (!program.collection) { program.help(); }
 
-var src = program.args[0];
-var dst = program.args[1];
-var parents = program.args.slice(2);
+var config = program.config
 
 // if relative, prepend current working dir
 if (config[0] !== '/') {
   config = process.cwd() + '/' + config;
 }
 
-config = require(config);
+config = properties.parse(fs.readFileSync(config, { encoding: 'utf8' }), { sections: true, namespaces: true });
 
-config.dbName = program.database || config.dbName;
-config.collection = program.collection || config.collection;
+var src = program.args[0];
+var dst = program.args[1];
+var parents = program.args.slice(2);
 
-if (!config.dbName) { program.help(); }
-if (!config.collection) { program.help(); }
+var collection = program.collection;
 
 var debug = !!program.verbose;
 
 function run(db) {
-  var vc = new VersionedCollection(db, config.collection, { debug: debug });
+  var vc = new VersionedCollection(db, collection, { debug: debug });
 
   var selector = { '_id._v': src, '_id._pe': program.pe || vc.localPerspective };
   vc._snapshotCollection.findOne(selector, function(err, item) {
@@ -108,8 +109,18 @@ function run(db) {
   });
 }
 
+var database = config.database;
+var dbCfg = {
+  dbName: database.name || 'local',
+  dbHost: database.path || database.host,
+  dbPort: database.port,
+  dbUser: database.username,
+  dbPass: database.password,
+  adminDb: database.adminDb
+};
+
 // open database
-_db(config, function(err, db) {
+_db(dbCfg, function(err, db) {
   if (err) { throw err; }
   run(db);
 });
