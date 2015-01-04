3eda0fa9a825c8b4d5c2eb2c06d19ce59811c7c8
Daan Kuijsten <code@kuijstenwebcompany.nl>
2015-01-04 18:29:09 +0100

    merge.js with ini config

--- bin/merge.js
+++ bin/merge.js
@@ -22,17 +22,19 @@
 
 var program = require('commander');
 var async = require('async');
+var properties = require('properties');
+var fs = require('fs');
 
 var _db = require('./_db');
 var VersionedCollection = require('../lib/versioned_collection');
 
 program
   .version('0.0.1')
-  .usage('[-v] -d database -c collection version1 version2 [versionLCA]')
+  .usage('[-v] -c collection -f config version1 version2 [versionLCA]')
   .description('merge two given versions, optionally using a third specified version as lca')
   .option('-d, --database <database>', 'name of the database')
   .option('-c, --collection <collection>', 'name of the collection to repair')
-  .option('    --config <file>', 'config file with db parameters, defaults to config/production.json')
+  .option('-f, --config <config>', 'an ini config file')
   .option('-p, --parents', 'set version1 and version2 to be the parents of the merge')
   .option('-s, --save', 'insert the new item in the snapshot')
   .option('-v, --verbose', 'verbose')
@@ -42,29 +44,27 @@ if (!program.args[1]) {
   program.help();
 }
 
-var config = program.config || __dirname + './../config/production.json';
+if (!program.config) { program.help(); }
+if (!program.collection) { program.help(); }
+
+var config = program.config
+var collection = program.collection;
+
+// if relative, prepend current working dir
+if (config[0] !== '/') {
+    config = process.cwd() + '/' + config;
+}
+
+config = properties.parse(fs.readFileSync(config, { encoding: 'utf8' }), { sections: true, namespaces: true });
 
 var v1 = program.args[0];
 var v2 = program.args[1];
 var lca = program.args[2];
 
-// if relative, prepend current working dir
-if (config[0] !== '/') {
-  config = process.cwd() + '/' + config;
-}
-
-config = require(config);
-
-config.dbName = program.database || config.dbName;
-config.collection = program.collection || config.collection;
-
-if (!config.dbName) { program.help(); }
-if (!config.collection) { program.help(); }
-
 var debug = !!program.verbose;
 
 function run(db) {
-  var vc = new VersionedCollection(db, config.collection, { debug: debug });
+  var vc = new VersionedCollection(db, collection, { debug: debug });
 
   var selector = { '_id._v': { $in: [ v1, v2 ] }, '_id._pe': vc.localPerspective };
   vc._snapshotCollection.find(selector).toArray(function(err, items) {
@@ -130,8 +130,18 @@ function run(db) {
   });
 }
 
+var database = config.database;
+var dbCfg = {
+  dbName: database.name || 'local',
+  dbHost: database.path || database.host,
+  dbPort: database.port,
+  dbUser: database.username,
+  dbPass: database.password,
+  adminDb: database.adminDb
+};
+
 // open database
-_db(config, function(err, db) {
+_db(dbCfg, function(err, db) {
   if (err) { throw err; }
   run(db);
 });
