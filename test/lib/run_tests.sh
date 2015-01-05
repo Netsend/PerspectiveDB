4c60a0ba2c9a7449fba409df9e55aa21926710b9
Tim Kuijsten <tim@netsend.nl>
2015-01-05 22:51:44 +0100

    run_tests.sh: add mocha tests

--- test/lib/run_tests.sh
+++ test/lib/run_tests.sh
@@ -3,17 +3,20 @@
 dir=$(dirname $0)
 cd "$dir" || exit 1
 
+echo "\nTEST mocha $i"
+mocha mocha/
+
 for i in `ls assert/*.js`; do
   echo "\nTEST node $i"
   node "$i"
 done
 
 for i in `ls assert_root/*.js`; do
-  echo "\nTEST node $i"
+  echo "\nTEST node root $i"
   node "$i"
 done
 
 for i in `ls mocha_root/*.js`; do
-  echo "\nTEST mocha $i"
+  echo "\nTEST mocha root $i"
   mocha "$i"
 done
