09afcaf9a90de4eafe072eaaec7ab5dd9330169e
Tim Kuijsten <tim@netsend.nl>
2015-01-05 22:49:45 +0100

    tests: rename run_root.sh to run_tests.sh

--- test/lib/run_tests.sh
+++ test/lib/run_tests.sh
@@ -3,6 +3,11 @@
 dir=$(dirname $0)
 cd "$dir" || exit 1
 
+for i in `ls assert/*.js`; do
+  echo "\nTEST node $i"
+  node "$i"
+done
+
 for i in `ls assert_root/*.js`; do
   echo "\nTEST node $i"
   node "$i"
