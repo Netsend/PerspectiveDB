bd75cb3aef3c7a3fb645f8d9dfa255fd14db813f
Tim Kuijsten <tim@netsend.nl>
2015-01-07 01:13:18 +0100

    bin/touch.js: work with new oplog pointer

--- bin/touch.js
+++ bin/touch.js
@@ -29,16 +29,15 @@ var _db = require('./_db');
 
 program
   .version('0.0.1')
-  .usage('[-r] -f config [database.collection]')
-  .description('set last used oplog item global or for a collection')
-  .option('-r, --remove', 'remove the timestamp')
+  .usage('-f config database.collection [timestamp]')
+  .description('set last oplog item or given timestamp on last item in given collection')
   .option('-f, --config  <config>', 'an ini config file')
   .option('-v, --verbose', 'verbose')
   .parse(process.argv);
 
 if (!program.config) { program.help(); }
 
-var config = program.config
+var config = program.config;
 
 // if relative, prepend current working dir
 if (config[0] !== '/') {
@@ -47,46 +46,57 @@ if (config[0] !== '/') {
 
 config = properties.parse(fs.readFileSync(config, { encoding: 'utf8' }), { sections: true, namespaces: true });
 
-var id = program.args[0] || 'lastUsedOplogItem';
+var ns = program.args[0];
+if (!ns) {
+  program.help();
+}
+
+var parts = ns.split('.');
+if (parts.length < 2) {
+  program.help();
+}
+
+var dbName = parts[0];
+var collName = 'm3.' + parts.slice(1).join('.');
 
 // update last used oplog item
 function run(db) {
-  var localDb = db.db(config.database.name || 'local');
-  if (program.remove) {
-    localDb.collection('m3').remove({ _id: id }, function(err, removed) {
-      if (err) {
-        console.error('error:', err.message);
-        process.exit(1);
-      }
-      if (removed < 1) {
-        console.log('item not found', id);
-        process.exit(0);
-      }
-      if (program.verbose) { console.log('removed', id, removed); }
-      localDb.close();
-      db.close();
+  function setOp(ts) {
+    var coll = db.collection(collName);
+    var update = { $set: { '_m3._op': ts } };
+    // fetch last id
+    coll.findOne({}, { sort: { $natural: -1 } }, function(err, lastItem) {
+      if (err) { throw err; }
+      db.collection(collName).update({ _id: lastItem._id }, update, function(err, updated) {
+        if (err) {
+          console.error('error', err, ts);
+          process.exit(1);
+        }
+        if (updated !== 1) {
+          console.error('error: while setting timestamp');
+          process.exit(1);
+        }
+        console.log(dbName, collName, 'set', ts);
+        db.close();
+      });
     });
+  }
+
+  var ts = program.args[1];
+  if (ts) {
+    ts = new Timestamp(ts);
+    setOp(ts);
   } else {
-    var ts = new Timestamp(0, (new Date()).getTime() / 1000);
-    localDb.collection('m3').update({ _id: id }, { $set: { ts: ts } }, { upsert: true }, function(err, updated) {
-      if (err) {
-        console.error('error:', err.message);
-        process.exit(1);
-      }
-      if (updated !== 1) {
-        console.error('error: while setting timestamp');
-        process.exit(1);
-      }
-      if (program.verbose) { console.log('set', id, ts); }
-      localDb.close();
-      db.close();
+    var oplogColl = db.db(config.database.oplogDb || 'local').collection(config.database.oplogCollection || 'oplog.$main');
+    oplogColl.findOne({}, { sort: { $natural: -1 } }, function(err, item) {
+      setOp(item.ts);
     });
   }
 }
 
 var database = config.database;
 var dbCfg = {
-  dbName: database.name || 'local',
+  dbName: dbName,
   dbHost: database.path || database.host,
   dbPort: database.port,
   dbUser: database.user,
