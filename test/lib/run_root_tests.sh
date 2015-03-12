471522fe089b9dfdffbd3e37847ed3888cea3a16
Tim Kuijsten <tim@netsend.nl>
2015-03-12 11:08:57 +0100

    split tests in root and non-root

---
+++ test/lib/run_root_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+dir=$(dirname $0)
+cd "$dir" || exit 1
+
+for i in `ls assert_root/*.js`; do
+  echo "\nTEST node root $i"
+  node "$i"
+done
+
+for i in `ls mocha_root/*.js`; do
+  echo "\nTEST mocha root $i"
+  mocha "$i"
+done
--- test/lib/run_tests.sh
+++ test/lib/run_tests.sh
@@ -10,13 +10,3 @@ for i in `ls assert/*.js`; do
   echo "\nTEST node $i"
   node "$i"
 done
-
-for i in `ls assert_root/*.js`; do
-  echo "\nTEST node root $i"
-  node "$i"
-done
-
-for i in `ls mocha_root/*.js`; do
-  echo "\nTEST mocha root $i"
-  mocha "$i"
-done
