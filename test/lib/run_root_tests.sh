47aeb115da7a0f8ebe523a0ffebd611271f28f16
Tim Kuijsten <tim@netsend.nl>
2015-07-09 22:25:39 +0200

    test/lib/run_root_tests.sh: run mocha first
    this works on first run, but any subsequent runs fail, not sure
    why yet, but preauth_exec seems to hang before
    mocha_root/versioned_system_root_listen.js test #7

--- test/lib/run_root_tests.sh
+++ test/lib/run_root_tests.sh
@@ -3,13 +3,13 @@
 dir=$(dirname $0)
 cd "$dir" || exit 1
 
-for i in `ls assert_root/*.js`; do
-  echo "\nTEST node root $i"
-  node "$i" || exit "$?"
-done
-
 for i in `ls mocha_root/*.js`; do
   echo "\nTEST mocha root $i"
   sleep 2
   mocha "$i" || exit "$?"
+done
+
+for i in `ls assert_root/*.js`; do
+  echo "\nTEST node root $i"
+  node "$i" || exit "$?"
 done
