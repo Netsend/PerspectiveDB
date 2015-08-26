2d49b8930ea02c03dd1261ab19452b10ceade810
Tim Kuijsten <tim@netsend.nl>
2015-08-26 15:03:00 +0200

    level_max test: to benchmark machine maximum

---
+++ test/lib/mocha/level_max.js
@@ -0,0 +1,135 @@
+/**
+ * Copyright 2015 Netsend.
+ *
+ * This file is part of Mastersync.
+ *
+ * Mastersync is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Mastersync is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with Mastersync. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+'use strict';
+
+var cp = require('child_process');
+var tmpdir = require('os').tmpdir;
+
+require('should');
+var rimraf = require('rimraf');
+var level = require('level');
+
+var db;
+var dbPath = tmpdir() + '/test_level_max';
+
+// open database
+before(function(done) {
+  rimraf(dbPath, function(err) {
+    if (err) { throw err; }
+    db = level(dbPath, { keyEncoding: 'binary', valueEncoding: 'binary' });
+    done();
+  });
+});
+
+after(function(done) {
+  db.close(function(err) {
+    if (err) { throw err; }
+    cp.execFile('du', ['-sh', dbPath], function(err, stdout, stderr) {
+      if (err) { throw err; }
+      if (stdout) { console.log(stdout); }
+      if (stderr) { console.error(stderr); }
+      rimraf(dbPath, done);
+    });
+  });
+});
+
+describe('levelmax', function() {
+  var seconds = 2;
+
+  it('insert 1 byte keys for ' + seconds + ' seconds (timeout)', function(done) {
+    this.timeout((seconds + 1) * 1000);
+
+    var run = true;
+    setTimeout(function() {
+      run = false;
+    }, seconds * 1000);
+
+    var i = 0;
+    function write() {
+      i++;
+      db.put(new Buffer(1), null, function(err) {
+        if (err) { throw err; }
+        if (run) {
+          write();
+        } else {
+          console.log('%d keys inserted in %d seconds (%d items per second)', i, seconds, i / seconds);
+          // expect at least 10.000 items per seconds
+          i.should.greaterThan(seconds * 10000);
+          done();
+        }
+      });
+    }
+
+    write();
+  });
+
+  it('insert 30 byte keys with 1 kilo-byte values for ' + seconds + ' seconds', function(done) {
+    this.timeout((seconds + 1) * 1000);
+
+    var run = true;
+    setTimeout(function() {
+      run = false;
+    }, seconds * 1000);
+
+    var i = 0;
+    function write() {
+      i++;
+      db.put(new Buffer(30), new Buffer(1024), function(err) {
+        if (err) { throw err; }
+        if (run) {
+          write();
+        } else {
+          console.log('%d keys inserted in %d seconds (%d items per second)', i, seconds, i / seconds);
+          // expect at least 10.000 items per seconds
+          i.should.greaterThan(seconds * 10000);
+          done();
+        }
+      });
+    }
+
+    write();
+  });
+
+  it('insert 30 byte keys with 1 kilo-byte values for ' + seconds + ' seconds (fsync)', function(done) {
+    this.timeout((seconds + 1) * 1000);
+
+    var run = true;
+    setTimeout(function() {
+      run = false;
+    }, seconds * 1000);
+
+    var i = 0;
+    function write() {
+      i++;
+      db.put(new Buffer(30), new Buffer(1024), { sync: true }, function(err) {
+        if (err) { throw err; }
+        if (run) {
+          write();
+        } else {
+          console.log('%d keys inserted in %d seconds (%d items per second)', i, seconds, i / seconds);
+          // expect at least 5.000 items per seconds
+          i.should.greaterThan(seconds * 5000);
+          done();
+        }
+      });
+    }
+
+    write();
+  });
+});
