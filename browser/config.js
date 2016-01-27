5e19cd23c81952753e6a30203c20f89c11411730
Tim Kuijsten <tim@netsend.nl>
2016-01-27 16:41:24 +0100

    browser: separate style and js for csp rules

---
+++ browser/config.js
@@ -0,0 +1,8 @@
+var config = {
+  name: 'theServer',
+  username: 'foo',
+  password: 'bar',
+  connect: 'wss://localhost:3344/baz',
+  import: true,
+  export: true
+};
---
+++ browser/config2.js
@@ -0,0 +1,8 @@
+var config = {
+  name: 'myServer',
+  username: 'fubar',
+  password: 'bar',
+  connect: 'wss://localhost:3344/baz',
+  import: true,
+  export: true
+};
