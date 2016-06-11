12a2e47e91b9e6346cc626ab111a667a405e5277
Tim Kuijsten <tim@netsend.nl>
2016-06-11 15:37:49 +0200

    refactor StreamMergeTree into Transform stream
    and integrate code into MergeTree.

--- test/lib/mocha/merge_tree.js
+++ test/lib/mocha/merge_tree.js
@@ -20,6 +20,7 @@
 
 var tmpdir = require('os').tmpdir;
 
+var bson = require('bson');
 var should = require('should');
 var rimraf = require('rimraf');
 var level = require('level-packager')(require('leveldown'));
@@ -30,6 +31,8 @@ var logger = require('../../../lib/logger');
 
 var db, cons, silence;
 var dbPath = tmpdir() + '/test_merge_tree';
+
+var BSON = new bson.BSONPure.BSON();
 
 // open database
 before(function(done) {
@@ -1912,14 +1915,534 @@ describe('MergeTree', function() {
     });
   });
 
-  describe('proxy', function() {
-    it('should proxy createReadStream', function() {
-      var mt = new MergeTree(db);
-      if (typeof mt.createReadStream !== 'function') {
-        throw new Error('createReadStream is not a function');
+  describe('createReadStream', function() {
+    var localName = '_local_createReadStream';
+    var stageName = '_stage_createReadStream';
+    var mt;
+    var perspective = 'I';
+
+    var A = {
+      h: { id: 'foo', v: 'Aaaa', pe: 'I', pa: [], i: 1 },
+      b: { baz : 'qux' }
+    };
+
+    var B = {
+      h: { id: 'foo', v: 'Bbbb', pe: 'I', pa: ['Aaaa'], i: 2 },
+      b: { foo: 'bar' }
+    };
+
+    var C = {
+      h: { id: 'foo', v: 'Cccc', pe: 'I', pa: ['Bbbb'], i: 3 },
+      b: {
+        baz : 'mux',
+        foo: 'bar'
       }
+    };
+
+    var D = {
+      h: { id: 'foo', v: 'Dddd', pe: 'I', pa: ['Cccc'], i: 4 },
+      b: {
+        baz : 'qux' }
+    };
+
+    var E = {
+      h: { id: 'foo', v: 'Eeee', pe: 'I', pa: ['Bbbb'], i: 5 },
+      b: { }
+    };
+
+    var F = {
+      h: { id: 'foo', v: 'Ffff', pe: 'I', pa: ['Eeee', 'Cccc'], i: 6 },
+      b: {
+        foo: 'bar' }
+    };
+
+    var G = {
+      h: { id: 'foo', v: 'Gggg', pe: 'I', pa: ['Ffff'], i: 7 },
+      b: { baz : 'qux' }
+    };
+
+    // same but without h.pe
+    var rA = {
+      h: { id: 'foo', v: 'Aaaa', pa: [] },
+      b: { baz : 'qux' }
+    };
+    var rB = {
+      h: { id: 'foo', v: 'Bbbb', pa: ['Aaaa'] },
+      b: { foo: 'bar' }
+    };
+    var rC = {
+      h: { id: 'foo', v: 'Cccc', pa: ['Bbbb'] },
+      b: {
+        baz : 'mux',
+        foo: 'bar'
+      }
+    };
+    var rD = {
+      h: { id: 'foo', v: 'Dddd', pa: ['Cccc'] },
+      b: { baz : 'qux' }
+    };
+    var rE = {
+      h: { id: 'foo', v: 'Eeee', pa: ['Bbbb'] },
+      b: {}
+    };
+    var rF = {
+      h: { id: 'foo', v: 'Ffff', pa: ['Eeee', 'Cccc'] },
+      b: { foo: 'bar' }
+    };
+    var rG = {
+      h: { id: 'foo', v: 'Gggg', pa: ['Ffff'] },
+      b: { baz : 'qux' }
+    };
+
+    // create the following structure:
+    // A <-- B <-- C <-- D
+    //        \     \
+    //         E <-- F <-- G
+
+    it('should create a MergeTree', function() {
+      mt = new MergeTree(db, {
+        local: localName,
+        stage: stageName,
+        vSize: 3,
+        log: silence
+      });
     });
 
+    it('should require opts.bson to be a boolean', function() {
+      (function() {
+        mt.createReadStream({ bson: {} });
+      }).should.throw('opts.bson must be a boolean');
+    });
+
+    it('should require opts.filter to be an object', function() {
+      (function() {
+        mt.createReadStream({ filter: 'foo' });
+      }).should.throw('opts.filter must be an object');
+    });
+
+    it('should require opts.first to be a base64 string or a number', function() {
+      (function() {
+        mt.createReadStream({ first: {} });
+      }).should.throw('opts.first must be a base64 string or a number');
+    });
+
+    it('should require opts.hooks to be an array', function() {
+      (function() {
+        mt.createReadStream({ hooks: {} });
+      }).should.throw('opts.hooks must be an array');
+    });
+
+    it('should require opts.hooksOpts to be an object', function() {
+      (function() {
+        mt.createReadStream({ hooksOpts: 'foo' });
+      }).should.throw('opts.hooksOpts must be an object');
+    });
+
+    it('should work with empty DAG', function(done) {
+      var smt = mt.createReadStream();
+      smt.on('data', function() { throw Error('no data should be emitted'); });
+      smt.on('end', done);
+    });
+
+    it('should save DAG', function(done) {
+      var ws = mt._local;
+      ws.write(A);
+      ws.write(B);
+      ws.write(C);
+      ws.write(D);
+      ws.write(E);
+      ws.write(F);
+      ws.write(G);
+      ws.end(done);
+    });
+
+    it('should return all elements', function(done) {
+      // use tailable is false to stop emitting documents after the last found doc
+      var smt = mt.createReadStream({ local: perspective });
+      var docs = [];
+
+      smt.on('data', function(doc) {
+        docs.push(doc);
+      });
+
+      smt.on('end', function() {
+        should.equal(docs.length, 7);
+        should.deepEqual(docs, [rA, rB, rC, rD, rE, rF, rG]);
+        done();
+      });
+    });
+
+    it('should tail and not close', function(done) {
+      // use tailable is false to stop emitting documents after the last found doc
+      var smt = mt.createReadStream({ local: perspective, log: silence, tail: true, tailRetry: 1 });
+      var docs = [];
+
+      smt.on('data', function(doc) {
+        docs.push(doc);
+      });
+
+      var closeCalled;
+      setTimeout(function() {
+        smt.end();
+        closeCalled = true;
+      }, 50);
+
+      smt.on('end', function() {
+        should.equal(docs.length, 7);
+        should.deepEqual(docs, [rA, rB, rC, rD, rE, rF, rG]);
+        should.strictEqual(closeCalled, true);
+        done();
+      });
+    });
+
+    it('should return bson buffer instances', function(done) {
+      // use tailable is false to stop emitting documents after the last found doc
+      var smt = mt.createReadStream({
+        local: perspective,
+        log: silence,
+        bson: true
+      });
+      var docs = [];
+
+      smt.on('data', function(doc) {
+        docs.push(BSON.deserialize(doc));
+      });
+
+      smt.on('end', function() {
+        should.equal(docs.length, 7);
+        should.deepEqual(docs, [rA, rB, rC, rD, rE, rF, rG]);
+        done();
+      });
+    });
+
+    it('should return only the last element if that is the offset', function(done) {
+      // use tailable is false to stop emitting documents after the last found doc
+      var smt = mt.createReadStream({
+        local: perspective,
+        log: silence,
+        first: G.h.v
+      });
+      var docs = [];
+
+      smt.on('data', function(doc) {
+        docs.push(doc);
+      });
+
+      smt.on('end', function() {
+        should.equal(docs.length, 1);
+        should.deepEqual(docs, [rG]);
+        done();
+      });
+    });
+
+    it('should return from offset E', function(done) {
+      // use tailable is false to stop emitting documents after the last found doc
+      var smt = mt.createReadStream({
+        local: perspective,
+        log: silence,
+        first: E.h.v
+      });
+      var docs = [];
+
+      smt.on('data', function(doc) {
+        docs.push(doc);
+      });
+
+      smt.on('end', function() {
+        should.equal(docs.length, 3);
+        should.deepEqual(docs[0], rE);
+        should.deepEqual(docs[1], rF);
+        should.deepEqual(docs[2], rG);
+        done();
+      });
+    });
+
+    it('should return everything since offset C (including E)', function(done) {
+      // use tailable is false to stop emitting documents after the last found doc
+      var smt = mt.createReadStream({
+        local: perspective,
+        log: silence,
+        first: C.h.v
+      });
+      var docs = [];
+
+      smt.on('data', function(doc) {
+        docs.push(doc);
+      });
+
+      smt.on('end', function() {
+        should.equal(docs.length, 5);
+        should.deepEqual(docs[0], rC);
+        should.deepEqual(docs[1], rD);
+        should.deepEqual(docs[2], rE);
+        should.deepEqual(docs[3], rF);
+        should.deepEqual(docs[4], rG);
+        done();
+      });
+    });
+
+    it('should return the complete DAG if filter is empty', function(done) {
+      // use tailable is false to stop emitting documents after the last found doc
+      var smt = mt.createReadStream({
+        local: perspective,
+        log: silence,
+        first: A.h.v
+      });
+      var docs = [];
+
+      smt.on('data', function(doc) {
+        docs.push(doc);
+      });
+
+      smt.on('end', function() {
+        should.equal(docs.length, 7);
+        should.deepEqual(docs[0], rA);
+        should.deepEqual(docs[1], rB);
+        should.deepEqual(docs[2], rC);
+        should.deepEqual(docs[3], rD);
+        should.deepEqual(docs[4], rE);
+        should.deepEqual(docs[5], rF);
+        should.deepEqual(docs[6], rG);
+        done();
+      });
+    });
+
+    it('should not endup with two same parents A for G since F is a merge but not selected', function(done) {
+      // should not find A twice for merge F
+      var smt = mt.createReadStream({
+        local: perspective,
+        log: silence,
+        first: A.h.v,
+        filter: { baz: 'qux' }
+      });
+      var docs = [];
+
+      smt.on('data', function(doc) {
+        docs.push(doc);
+      });
+
+      smt.on('end', function() {
+        should.equal(docs.length, 3);
+        should.deepEqual(docs[0], { h: { id: 'foo', v: 'Aaaa', pa: [] }, b: { baz : 'qux' } });
+        should.deepEqual(docs[1], { h: { id: 'foo', v: 'Dddd', pa: ['Aaaa'] },  b: { baz : 'qux' } });
+        should.deepEqual(docs[2], { h: { id: 'foo', v: 'Gggg', pa: ['Aaaa'] },  b: { baz : 'qux' } });
+        done();
+      });
+    });
+
+    it('should return only attrs with baz = mug and change root to C', function(done) {
+      // should not find A twice for merge F
+      var smt = mt.createReadStream({
+        local: perspective,
+        log: silence,
+        first: A.h.v,
+        filter: { baz: 'mux' }
+      });
+      var docs = [];
+
+      smt.on('data', function(doc) {
+        docs.push(doc);
+      });
+
+      smt.on('end', function() {
+        should.equal(docs.length, 1);
+        should.deepEqual(docs[0], { h: { id: 'foo', v: 'Cccc', pa: [] }, b: { baz : 'mux', foo: 'bar' } });
+        done();
+      });
+    });
+
+    it('should return only attrs with foo = bar and change root to B and alter subsequent parents', function(done) {
+      // should not find A twice for merge F
+      var smt = mt.createReadStream({
+        local: perspective,
+        log: silence,
+        first: A.h.v,
+        filter: { foo: 'bar' }
+      });
+      var docs = [];
+
+      smt.on('data', function(doc) {
+        docs.push(doc);
+      });
+
+      smt.on('end', function() {
+        should.equal(docs.length, 3);
+        should.deepEqual(docs[0], { h: { id: 'foo', v: 'Bbbb', pa: [] }, b: { foo: 'bar' } });
+        should.deepEqual(docs[1], { h: { id: 'foo', v: 'Cccc', pa: ['Bbbb'] }, b: { baz: 'mux', foo: 'bar' } });
+        should.deepEqual(docs[2], { h: { id: 'foo', v: 'Ffff', pa: ['Bbbb', 'Cccc'] }, b: { foo: 'bar' } });
+        done();
+      });
+    });
+
+    it('should return nothing if filters don\'t match any item', function(done) {
+      // should not find A twice for merge F
+      var smt = mt.createReadStream({
+        local: perspective,
+        log: silence,
+        first: A.h.v,
+        filter: { some: 'none' }
+      });
+      var docs = [];
+
+      smt.on('data', function(doc) {
+        docs.push(doc);
+      });
+
+      smt.on('end', function() {
+        should.equal(docs.length, 0);
+        done();
+      });
+    });
+
+    it('should execute each hook', function(done) {
+      // should not find A twice for merge F
+      function transform(db, obj, opts, cb) {
+        delete obj.b.baz;
+        obj.b.checked = true;
+        if (i === 1) {
+          obj.b.checked = false;
+        }
+        i++;
+        cb(null, obj);
+      }
+      function hook1(db, obj, opts, cb) {
+        obj.b.hook1 = true;
+        if (obj.h.v === 'Gggg') { obj.b.hook1g = 'foo'; }
+        cb(null, obj);
+      }
+      function hook2(db, obj, opts, cb) {
+        if (obj.b.hook1) {
+          obj.b.hook2 = true;
+        }
+        cb(null, obj);
+      }
+
+      var smt = mt.createReadStream({
+        local: perspective,
+        log: silence,
+        first: A.h.v,
+        filter: { baz: 'qux' },
+        hooks: [transform, hook1, hook2]
+      });
+      var i = 0;
+
+      var docs = [];
+
+      smt.on('data', function(doc) {
+        docs.push(doc);
+      });
+
+      smt.on('end', function() {
+        should.equal(docs.length, 3);
+        should.deepEqual(docs[0], { h: { id: 'foo', v: 'Aaaa', pa: [] }, b: { checked : true, hook1: true, hook2: true } });
+        should.deepEqual(docs[1], { h: { id: 'foo', v: 'Dddd', pa: ['Aaaa'] }, b: { checked : false, hook1: true, hook2: true} });
+        should.deepEqual(docs[2], { h: { id: 'foo', v: 'Gggg', pa: ['Aaaa'] }, b: { checked : true, hook1g: 'foo', hook1: true, hook2: true} });
+        done();
+      });
+    });
+
+    it('should cancel hook execution and skip item if one hook filters', function(done) {
+      // should not find A twice for merge F
+
+      function transform(db, obj, opts, callback) {
+        delete obj.b.baz;
+        obj.b.transformed = true;
+        callback(null, obj);
+      }
+      // filter F. G should get parents of F which are E and C
+      function hook1(db, obj, opts, callback) {
+        if (obj.h.v === 'Ffff') {
+          return callback(null, null);
+        }
+        callback(null, obj);
+      }
+      function hook2(db, obj, opts, callback) {
+        obj.b.hook2 = true;
+        callback(null, obj);
+      }
+
+      var smt = mt.createReadStream({
+        local: perspective,
+        log: silence,
+        first: E.h.v,
+        hooks: [transform, hook1, hook2]
+      });
+
+      var docs = [];
+
+      smt.on('data', function(doc) {
+        docs.push(doc);
+      });
+
+      smt.on('end', function() {
+        should.equal(docs.length, 2);
+        should.deepEqual(docs[0], { h: { id: 'foo', v: 'Eeee', pa: ['Bbbb'] }, b: { transformed : true, hook2: true } });
+        should.deepEqual(docs[1], { h: { id: 'foo', v: 'Gggg', pa: ['Eeee', 'Cccc'] }, b: { transformed : true, hook2: true} });
+        done();
+      });
+    });
+
+    it('should return only attrs with foo = bar and change root to B and alter subsequent parents, filtered by hook', function(done) {
+      function hook(db, obj, opts, callback) {
+        if (obj.b.foo === 'bar') {
+          return callback(null, obj);
+        }
+        callback(null, null);
+      }
+
+      var smt = mt.createReadStream({
+        local: perspective,
+        log: silence,
+        first: A.h.v,
+        hooks: [hook]
+      });
+      var docs = [];
+
+      smt.on('data', function(doc) {
+        docs.push(doc);
+      });
+
+      smt.on('end', function() {
+        should.equal(docs.length, 3);
+        should.deepEqual(docs[0], { h: { id: 'foo', v: 'Bbbb', pa: [] }, b: { foo: 'bar' } });
+        should.deepEqual(docs[1], { h: { id: 'foo', v: 'Cccc', pa: ['Bbbb'] }, b: { baz: 'mux', foo: 'bar' } });
+        should.deepEqual(docs[2], { h: { id: 'foo', v: 'Ffff', pa: ['Bbbb', 'Cccc'] }, b: { foo: 'bar' } });
+        done();
+      });
+    });
+
+    it('should return only attrs with foo = bar and change root to B and alter subsequent parents, filtered by hook and offset = B', function(done) {
+      function hook(db, obj, opts, callback) {
+        if (obj.b.foo === 'bar') {
+          return callback(null, obj);
+        }
+        callback(null, null);
+      }
+
+      var smt = mt.createReadStream({
+        local: perspective,
+        log: silence,
+        first: B.h.v,
+        hooks: [hook]
+      });
+
+      var docs = [];
+
+      smt.on('data', function(doc) {
+        docs.push(doc);
+      });
+
+      smt.on('end', function() {
+        should.equal(docs.length, 3);
+        should.deepEqual(docs[0], { h: { id: 'foo', v: 'Bbbb', pa: [] }, b: { foo: 'bar' } });
+        should.deepEqual(docs[1], { h: { id: 'foo', v: 'Cccc', pa: ['Bbbb'] }, b: { baz: 'mux', foo: 'bar' } });
+        should.deepEqual(docs[2], { h: { id: 'foo', v: 'Ffff', pa: ['Bbbb', 'Cccc'] }, b: { foo: 'bar' } });
+        done();
+      });
+    });
+  });
+
+  describe('proxy', function() {
     it('should proxy getByVersion', function() {
       var mt = new MergeTree(db);
       if (typeof mt.getByVersion !== 'function') {
