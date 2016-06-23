d43baa51eabca2e5ca8c0671511b68dd71184a48
Tim Kuijsten <tim@netsend.nl>
2016-06-23 22:41:25 +0200

    fix preauth_exec tests

--- test/lib/assert_root/preauth_exec_root.js
+++ test/lib/assert_root/preauth_exec_root.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014-2016 Netsend.
+ * Copyright 2014, 2015, 2016 Netsend.
  *
  * This file is part of PersDB.
  *
@@ -25,78 +25,17 @@ if (process.getuid() !== 0) {
 
 var assert = require('assert');
 var net = require('net');
-var childProcess = require('child_process');
 var fs = require('fs');
 var c = require('constants');
 
 var async = require('async');
 var ws = require('nodejs-websocket');
-var xtend = require('xtend');
 
-var noop = require('../../../lib/noop');
+var logger = require('../../../lib/logger');
+var spawn = require('../../lib/spawn');
 
 var tasks = [];
 var tasks2 = [];
-
-function spawn(options, spawnOpts) {
-  var opts = xtend({
-    configBase: __dirname,
-    config: 'test_persdb.hjson',
-    echoOut: false,
-    echoErr: true,
-    onSpawn: noop,
-    onMessage: null,                                          // handle ipc messages
-    onExit: noop,
-    exitCode: 0,                                              // test if exit code is 0
-    exitSignal: null,                                         // test if exit signal is empty
-    testStdout: null,
-    testStderr: function(s) {                                 // test if stderr is empty
-      if (s.length) { throw new Error(s); }
-    }
-  }, options);
-
-  var sOpts = xtend({
-    args: [__dirname + '/../../../lib/preauth_exec'],
-    stdio: ['pipe', 'pipe', 'pipe', 'ipc']
-  }, spawnOpts);
-
-  // print line number
-  console.log('test #%d', new Error().stack.split('\n')[2].match(/preauth_exec_root.js:([0-9]+):[0-9]+/)[1]);
-
-  var extraArgs = [];
-  var child = childProcess.spawn(process.execPath, sOpts.args.concat(extraArgs), sOpts);
-
-  if (opts.echoOut) { child.stdout.pipe(process.stdout); }
-  if (opts.echoErr) { child.stderr.pipe(process.stderr); }
-
-  var stdout = '';
-  var stderr = '';
-  child.stdout.setEncoding('utf8');
-  child.stderr.setEncoding('utf8');
-  child.stdout.on('data', function(data) { stdout += data; });
-  child.stderr.on('data', function(data) { stderr += data; });
-
-  child.on('exit', function(code, sig) {
-    if (opts.testStdout) {
-      opts.testStdout(stdout);
-    }
-    if (opts.testStderr) {
-      opts.testStderr(stderr);
-    }
-    assert.strictEqual(code, opts.exitCode);
-    assert.strictEqual(sig, opts.exitSignal);
-    opts.onExit(null, code, sig, stdout, stderr);
-  });
-
-  if (opts.onMessage) {
-    child.on('message', function(msg) {
-      opts.onMessage(msg, child);
-    });
-  }
-  opts.onSpawn(child);
-}
-
-var logger = require('../../../lib/logger');
 
 var cons, silence;
 
@@ -127,7 +66,7 @@ tasks.push(function(done) {
     }
   }
 
-  spawn({
+  var opts = {
     onMessage: onMessage,
     onExit: done,
     echoErr: false,
@@ -135,7 +74,9 @@ tasks.push(function(done) {
     testStderr: function(stderr) {
       assert(/msg.log must be an object/.test(stderr));
     }
-  });
+  };
+
+  spawn([__dirname + '/../../../lib/preauth_exec', __dirname + '/test_persdb.hjson'], opts);
 });
 
 // should require msg.port to be a number
@@ -153,7 +94,7 @@ tasks.push(function(done) {
     }
   }
 
-  spawn({
+  var opts = {
     onMessage: onMessage,
     onExit: done,
     echoErr: false,
@@ -161,11 +102,13 @@ tasks.push(function(done) {
     testStderr: function(stderr) {
       assert(/msg.port must be a number/.test(stderr));
     }
-  });
+  };
+
+  spawn([__dirname + '/../../../lib/preauth_exec', __dirname + '/test_persdb.hjson'], opts);
 });
 
 // should disconnect after receiving more than 1024 bytes of data without newlines
-tasks2.push(function(done) {
+tasks.push(function(done) {
   function onMessage(msg, child) {
     switch (msg) {
     case 'init':
@@ -175,8 +118,6 @@ tasks2.push(function(done) {
       });
       break;
     case 'listen':
-      assert(/preauth changed root to \/var\/empty and user:group to _pdbnull:_pdbnull/.test(child.stdout));
-
       var ms = net.createConnection(1234);
 
       var pattern = 'abcdefgh';
@@ -188,7 +129,7 @@ tasks2.push(function(done) {
             if (err) {
               assert(/EPIPE|This socket is closed|This socket has been ended by the other party/.test(err.message));
               assert(i >= 125 && i <= 135);
-              child.kill();
+              child.send({ type: 'kill' });
             } else {
               flood();
             }
@@ -207,16 +148,20 @@ tasks2.push(function(done) {
     }
   }
 
-  spawn({
+  var opts = {
     onMessage: onMessage,
     onExit: done,
-    echoErr: true,
+    echoErr: false,
+    testStdout: function(stdout) {
+      assert(/preauth changed root to \/var\/empty and user:group to _pdbnull:_pdbnull/.test(stdout));
+    },
     testStderr: function(stderr) {
       assert(/Error: more than maxBytes received/.test(stderr));
     }
-  });
-});
+  };
 
+  spawn([__dirname + '/../../../lib/preauth_exec', __dirname + '/test_persdb.hjson'], opts);
+});
 
 // should not parse objects larger than max length
 tasks.push(function(done) {
@@ -253,7 +198,7 @@ tasks.push(function(done) {
     }
   }
 
-  spawn({
+  var opts = {
     onMessage: onMessage,
     onExit: done,
     echoErr: false,
@@ -263,7 +208,8 @@ tasks.push(function(done) {
     testStderr: function(stderr) {
       assert(/Error: more than maxBytes received/.test(stderr));
     }
-  });
+  };
+  spawn([__dirname + '/../../../lib/preauth_exec', __dirname + '/test_persdb.hjson'], opts);
 });
 
 // should pass auth request to parent
@@ -297,10 +243,12 @@ tasks.push(function(done) {
     }
   }
 
-  spawn({
+  var opts = {
     onMessage: onMessage,
     onExit: done
-  });
+  };
+
+  spawn([__dirname + '/../../../lib/preauth_exec', __dirname + '/test_persdb.hjson'], opts);
 });
 
 // should start a secure websocket server and pass auth request to parent
@@ -362,13 +310,15 @@ tasks.push(function(done) {
     }
   }
 
-  spawn({
+  var opts = {
     onMessage: onMessage,
     onExit: done,
     testStdout: function(stdout) {
       assert(/wss changed root to \/var\/empty and user:group to _pdbnull:_pdbnull/.test(stdout));
     }
-  });
+  };
+
+  spawn([__dirname + '/../../../lib/preauth_exec', __dirname + '/test_persdb.hjson'], opts);
 });
 
 async.series(tasks, function(err) {
