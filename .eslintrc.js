9e2c854dc480846cf113de0810b64bd925032d02
Tim Kuijsten <tim@netsend.nl>
2016-08-05 20:39:56 +0200

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,23 @@
+'use strict';
+
 module.exports = {
-  "env": {
-    "es6": true,
-    "node": true
+  env: {
+    es6: true,
+    node: true
   },
-  "globals": {
-    "WebSocket": true
+  globals: {
+    WebSocket: true,
+    indexedDB: true
   },
-  "extends": "eslint:recommended",
-  "rules": {
-    "no-console": "off",
-    "indent": [ "error", 2 ],
-    "linebreak-style": [ "error", "unix" ],
-    "quotes": [ "error", "single" ],
-    "strict": [ "error", "safe" ],
-    'comma-dangle': ["error", "only-multiline"]
-    //"semi": [ "error", "never" ],
-    //"semi-spacing": ["error" ]
+  'extends': 'eslint:recommended',
+  rules: {
+    'no-console': 'off',
+    indent: [ 'error', 2 ],
+    linebreak-style: [ 'error', 'unix' ],
+    quotes: [ 'error', 'single' ],
+    strict: [ 'error', 'safe' ],
+    'comma-dangle': ['error', 'only-multiline']
+    //'semi': [ 'error', 'never' ],
+    //'semi-spacing': ['error' ]
   }
 };
